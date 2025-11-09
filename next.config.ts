8883ac610770d99a1252c403242d878ecb2bbcbc
kimestelle <137699667+kimestelle@users.noreply.github.com>
2025-11-09 17:00:16 -0500

    next deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import path from "path";
+
+const repo = "textellation";
+const base = `/${repo}`;
 
 const nextConfig: NextConfig = {
-  /* config options here */
 };
 
 export default nextConfig;
